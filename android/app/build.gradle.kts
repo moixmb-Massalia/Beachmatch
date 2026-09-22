6bfa6c7b4f298c98ea2860810c72abd3dce65638
moixmb-Massalia <moixmb@gmail.com>
2026-09-22 16:01:14 +0200

    release(android): bump to 1.0.25+125, configure targetSdk 35 and POST_NOTIFICATIONS for Google Play compliance

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,12 +19,9 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.beachmatch.app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
@@ -37,7 +34,9 @@ android {
                 keystoreProperties.load(FileInputStream(keystorePropertiesFile))
                 keyAlias = keystoreProperties["keyAlias"] as String
                 keyPassword = keystoreProperties["keyPassword"] as String
-                storeFile = file(keystoreProperties["storeFile"] as String)
+                val storeFilePath = keystoreProperties["storeFile"] as String
+                val appKeystore = rootProject.file("app/$storeFilePath")
+                storeFile = if (appKeystore.exists()) appKeystore else file(storeFilePath)
                 storePassword = keystoreProperties["storePassword"] as String
             }
         }
