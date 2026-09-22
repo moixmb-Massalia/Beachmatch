de7e581211bd0a0fb318d854c74ffa518feb5119
moixmb-Massalia <moixmb@gmail.com>
2026-09-22 16:12:47 +0200

    release(android): upgrade targetSdk to 36 and bump version to 1.0.26+126 for Google Play Store compliance

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
     defaultConfig {
         applicationId = "com.beachmatch.app"
         minSdk = flutter.minSdkVersion
-        targetSdk = 35
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
